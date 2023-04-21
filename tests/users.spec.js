1150151268611f9a10dec594e946987803ec3f4f
Dan Balarin <dan.balarin@gmail.com>
2023-04-21 20:37:55 +0200

    test: add login and logout tests

--- tests/users.spec.js
+++ tests/users.spec.js
@@ -46,3 +46,32 @@ test.serial('POST /register after registration username is visible', async (t) =
 
   t.assert(response.text.includes('adam'))
 })
+
+test.serial('GET /login it shows login form', async (t) => {
+  const response = await supertest(app).get('/login')
+
+  t.assert(response.text.includes('Prihlaseni'))
+})
+
+test.serial('POST /login after login username is visible', async (t) => {
+  await createUser('username', 'password')
+  const agent = supertest.agent(app)
+
+  const response = await agent.post('/login').type('form').send({ username: 'username', password: 'password' }).redirects(1)
+
+  t.assert(response.text.includes('username'))
+})
+
+test.serial('GET /logout after logout username is not visible', async (t) => {
+  await createUser('username', 'password')
+  const agent = supertest.agent(app)
+
+  let response = await agent.post('/login').type('form').send({ username: 'username', password: 'password' }).redirects(1)
+
+  t.assert(response.text.includes('username'))
+
+  response = await agent.get('/logout').redirects(1)
+
+  t.assert(!response.text.includes('adam'))
+  t.assert(response.text.includes('Prihlaseni'))
+})
